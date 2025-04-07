0bf05ca2431fd2a87c47c8910477d4bcf48fef3e
ajoshia4 <156046985+ajoshia4@users.noreply.github.com>
2025-04-07 16:54:16 -0400

    Create day3.sv

---
+++ 21_days_of_RTL/day3_edge_detector/day3.sv
@@ -0,0 +1,23 @@
+ // An edge detector
+
+module day3 (
+  input     wire    clk,
+  input     wire    reset,
+
+  input     wire    a_i,
+
+  output    wire    rising_edge_o,
+  output    wire    falling_edge_o
+);
+  reg q;
+
+  // Write your logic here...
+  always @(posedge clk) begin
+    if(!reset) q<=0;
+    else q<=a_i;
+  end
+  
+  assign rising_edge_o = a_i & ~q;
+  assign falling_edge_o = ~a_i & q;
+  
+endmodule
